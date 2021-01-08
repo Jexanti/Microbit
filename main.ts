6b492215feb0dfa62f8b8048ec293af898bc68c9
Jexanti <76551875+Jexanti@users.noreply.github.com>
2021-01-08 14:18:59 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -6,6 +6,14 @@ basic.forever(function () {
     for (let index = 0; index < 4; index++) {
         basic.showIcon(IconNames.Square)
         basic.showIcon(IconNames.SmallSquare)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+        basic.showIcon(IconNames.SmallSquare)
     }
     basic.showIcon(IconNames.Square)
 })
